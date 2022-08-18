a33ecfc43ba6f5121b764d0193047f7da0611444
Andrey1914 <anlikajw@gmail.com>
2022-08-18 20:34:12 +0200

    add styles

--- src/components/Button/ButtonStyled.jsx
+++ src/components/Button/ButtonStyled.jsx
@@ -1,13 +1,12 @@
 import styled from '@emotion/styled';
 
 export const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: ${p => p.theme.space[7]}px;
-  height: ${p => p.theme.space[5]}px;
   border-radius: ${p => p.theme.radii.small};
-  padding: 0;
-  /* padding: ${p => p.theme.space[2]}px; */
-  /* padding-left: ${p => p.theme.space[4]}px;
-  padding-right: ${p => p.theme.space[4]}px; */
+  padding: ${p => p.theme.space[3]}px;
   cursor: pointer;
   border: ${p => p.theme.borders.none};
   box-shadow: 0px 3px 5px 1px rgba(45, 90, 124, 0.2);
